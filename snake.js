2cb93bcda7ceb8e62148d9e9ccd3b7a46352dc39
Vatsal Gala <31574642+Vatsal-Gala@users.noreply.github.com>
2018-04-02 17:07:13 +0530

    Add files via upload

--- snake.js
+++ snake.js
@@ -1,32 +1,74 @@
-var size = 10;
 var speed = .09;
-var fx ,fy;
-var lifespan = 255.0;
+var ballSize = 20;
+var width,height;
+var missiles = [];
+var totalballs = 3;
+
+function missile(x,y){
+  this.x = x;
+  this.y = y;
+  this.lifespan = random(1,3)*255;
+  this.speed = random(1,10)*0.005;
+}
+
 function setup() {
-  createCanvas(800, 400);
-  fx = random(0,800);
-  fy = random(0,400);
+
+  width = 700;
+  height = 700;
+  createCanvas(700, height);
   fill(255);
-  ellipse(fx, fy, 20, 20);
+  for (var i=0;i<totalballs;i++){
+    missiles.push(newMissile());
+  }
+  console.log(missiles);
+
 }
 
 function draw() {
   background(51);
   spawn();
-  fx += (mouseX-fx)*speed;
-  fy += (mouseY-fy)*speed;
+  detect_collision();
+}
+
+function spawn(item,index){
+  
+  // Map function implementation
+
+  for (var i=0;i<totalballs;i++){
+    m=missiles[i];
+    if (!(m.lifespan>51)){
+      idx = missiles.indexOf(m);
+      m = newMissile();
+    }
+    else{
+      m.x += (mouseX-m.x)*m.speed;
+      m.y += (mouseY-m.y)*m.speed;
+      m.lifespan -=2;
+      fill(m.lifespan);
+      ellipse(m.x,m.y, ballSize, ballSize);
+      stroke(51);
+      // console.log("Bye");
+      // console.log(m.lifespan);
+    }
+    missiles[i] = m;
+    if(detect_collision(m.x, m.y)){
+      alert("Dead");
+      missiles[i].lifespan = 0;
+    }
+  }
 
 }
 
-function spawn(){
-  stroke(51);
-  fill(lifespan-=2);
-  if(lifespan > 51)
-    ellipse(fx, fy, 20, 20);
-  else{
-    fx = random(0,800);
-    fy = random(0,400);
-    lifespan = 255.0;
-    ellipse(fx,fy,20,20);
+function detect_collision(x,y){
+    if (Math.sqrt(Math.pow((x-mouseX),2)+Math.pow((y-mouseY),2)) <= ballSize){
+      return true;
     }
+    return false;
+}
+
+function newMissile(){
+    fx = random(0,width);
+    fy = random(0,height);
+    m = new missile(fx,fy);
+    return m;
 }
