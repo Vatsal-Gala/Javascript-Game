631b8bc5f7335b3f132fa177e8650feea31e3df3
Vatsal Gala <31574642+Vatsal-Gala@users.noreply.github.com>
2018-04-03 15:46:13 +0530

    Progress Bar and score.
    The player can cross boundaries with jumping back on the other side.

--- snake.js
+++ snake.js
@@ -7,6 +7,7 @@ var totalballs = 3;
 var ship;
 var skore = 0;
 var hits = 0;
+var highscore = 0;
 
 function missile(x,y){
   this.x = x;
@@ -20,8 +21,8 @@ function spaceship(x,y){
   this.y = y;
   this.vx = 0;
   this.vy = 0;
-  this.h = 22;
-  this.w = 8;
+  this.h = 15;
+  this.w = 15;
   this.angle = Math.PI/2;
   this.crashed = false;
   this.color = 255;
@@ -32,11 +33,11 @@ function setup() {
 
   width = 700;
   height = 700;
-  createCanvas(700, height);
+  createCanvas(500, 500);
   fill(255);
   ship = new spaceship(width/2,height/2);
   drawSpaceship();
-  for (var i=0;i<totalballs;i++){
+  for (var i=0;i<100;i++){
     missiles.push(newMissile());
   }
   console.log(missiles);
@@ -61,8 +62,8 @@ function spawn(item,index){
       m = newMissile();
     }
     else{
-      m.x += (ship.x-m.x)*m.speed;
-      m.y += (ship.y-m.y)*m.speed;
+      m.x = (500 + m.x + (ship.x-m.x)*m.speed)%500;
+      m.y = (500 + m.y + (ship.y-m.y)*m.speed)%500;
       m.lifespan -=2;
       fill(m.lifespan);
       stroke(51);
@@ -81,6 +82,7 @@ function spawn(item,index){
 function detect_collision(x,y){
     if (Math.sqrt(Math.pow((x-ship.x),2)+Math.pow((y-ship.y),2)) <= ballSize){
       hits ++;
+      boom();
       return true;
     }
     return false;
@@ -102,32 +104,70 @@ function drawSpaceship(){
 
 function updateShip(){
   if (keyIsDown(LEFT_ARROW)) {
-    ship.x -= shipSpeed;
+    ship.x = (500 + ship.x - shipSpeed)%500;
+    fill("ORANGE");
+    triangle(ship.x-ship.w/2    , ship.y-ship.h/2,
+             ship.x-ship.w/2    , ship.y+ship.h/2,
+             ship.x+ship.w/2+20 , ship.y);
   } else if (keyIsDown(RIGHT_ARROW)) {
-    ship.x += shipSpeed;
+    ship.x = (500 + ship.x + shipSpeed)%500;
+    fill("ORANGE");
+    triangle(ship.x+ship.w/2    , ship.y-ship.h/2,
+             ship.x+ship.w/2    , ship.y+ship.h/2,
+             ship.x-ship.w/2-20 , ship.y);
   }
   if (keyIsDown(UP_ARROW)){
-    ship.y -= shipSpeed;
+    ship.y = (500 + ship.y - shipSpeed)%500;
+    fill("ORANGE");
+    triangle(ship.x-ship.w/2    , ship.y-ship.h/2,
+             ship.x+ship.w/2    , ship.y-ship.h/2,
+             ship.x             , ship.y+ship.h/2+20);
   } else if (keyIsDown(DOWN_ARROW)){
-    ship.y += shipSpeed;
+    ship.y = (500 + ship.y + shipSpeed)%500;
+    fill("ORANGE");
+    triangle(ship.x-ship.w/2    , ship.y+ship.h/2,
+             ship.x+ship.w/2    , ship.y+ship.h/2,
+             ship.x             , ship.y-ship.h/2-20);
   }
 }
 
 function count() {
   skore += 1/30;
+  highscore = highscore>skore?highscore:skore;
+  totalballs = Math.ceil(skore/10);
   fill("WHITE");
   text('Score : '+Math.ceil(skore),20,20);
+  text("Highscore : "+Math.ceil(highscore), 20,40);
   if (11 - hits > 0){
     rectMode(CORNER);
     fill("WHITE");
     stroke("WHITE");
-    rect(width - 100, 10 , 50 , 20);
+    rect(width - 150, 20 , 100 , 10);
     fill("RED");
     stroke("WHITE");
-    rect(width - 100, 10 , 5*(10-hits), 20 );
+    rect(width - 150, 20 , 10*(10-hits), 10 );
+    fill("WHITE");
+    text(10-hits,width - 45, 30);
   }
   if (hits == 11){
     hits++;
-    alert("Game Over !");
+    alert("Game Over !"+"\nYour Score is : "+Math.ceil(skore));
+    refresh();
   }
+}
+
+function refresh(){
+  speed = .09;
+  ballSize = 20;
+  shipSpeed = 4;
+  missiles = [];
+  totalballs = 3;
+  ship;
+  skore = 0;
+  hits = 0;
+  setup();
+}
+
+function boom(){
+  background("RED");
 }
